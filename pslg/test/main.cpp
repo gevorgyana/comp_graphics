4148c78b16ed9eb2d3064d16aab1b3cdb75e6e22
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-03-06 01:51:36 +0200

    blabla

--- pslg/test/main.cpp
+++ pslg/test/main.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <cmath>
+
+using std::cout;
+using std::endl;
+
 /**
    tree used for reference
 
@@ -22,3 +28,39 @@
     with step = -2
 
  */
+
+// find the max power of 2 in @x
+int p2(int x)
+{
+  int p = 0;
+  int pp = 1;
+  while (x % static_cast<int>(pow(2, pp)) == 0)
+  {
+    ++p;
+    pp *= 2;
+  }
+  return p;
+}
+
+/**
+ * returns the parent of x in standard search heirarchy
+ */
+int move(int x)
+{
+  int standard_x = p2(x);
+  int next_x = standard_x;
+  int to_the_right = true;
+  while (next_x != x)
+  {
+    to_the_right = false;
+    next_x += standard_x * 2;
+  }
+}
+
+int main()
+{
+  cout << endl;
+  cout << "application is running" << endl;
+  cout << p2(2);
+  return 0;
+}
