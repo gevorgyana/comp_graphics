edd1532274ecb2a50cc2d6c5601e7b2b3cd40c54
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-05-17 01:11:22 +0300

    Fix comments. Fix Rust compiler warnings and errors related to invalid types.

--- point_location/lies_where_in_planar_graph/using_slabs_decomposition/src/library_types.rs
+++ point_location/lies_where_in_planar_graph/using_slabs_decomposition/src/library_types.rs
@@ -1,7 +1,7 @@
-use std::collections::{BTreeSet, HashSet};
-use geo::{Line, Coordinate};
+use geo::{Line};
 use std::cmp::Ordering::{self, Less, Greater};
 
+#[derive(Copy, Clone)]
 pub struct CompLine {
     pub val : Line<f64>,
 }
@@ -24,28 +24,5 @@ impl Eq for CompLine {}
 impl PartialEq for CompLine {
     fn eq(&self, other : &Self) -> bool {
         self.val.start.x == other.val.start.x
-    }
-}
-
-/**
- * this structure contains the slabs
- * and I define searching methods on it
- * later in this code
- */
-
-#[allow(unused)]
-pub struct SearchStructure {
-    pub data : Option<Vec<CompLine>>,
-    pub slabs : Option<Vec<CompLine>>,
-    pub tree : Option<BTreeSet<CompLine>>,
-}
-
-impl Default for SearchStructure {
-    fn default() -> Self {
-        Self {
-            data : None,
-            slabs : None,
-            tree : None
-        }
     }
 }
--- point_location/lies_where_in_planar_graph/using_slabs_decomposition/src/main.rs
+++ point_location/lies_where_in_planar_graph/using_slabs_decomposition/src/main.rs
@@ -1,8 +1,34 @@
-use geo::{Line, Coordinate};
+use geo::{Coordinate};
+use std::collections::BTreeSet;
 
 mod library_types;
+use crate::library_types as types;
 
-impl library_types::SearchStructure {
+/**
+ * this structure contains the slabs
+ * and I define searching methods on it
+ * later in this code
+ */
+
+#[allow(unused)]
+pub struct SearchStructure {
+    pub data : Option<Vec<library_types::CompLine>>,
+    pub slabs : Option<Vec<library_types::CompLine>>,
+    pub tree : Option<BTreeSet<library_types::CompLine>>,
+}
+
+impl Default for SearchStructure {
+    fn default() -> Self {
+        Self {
+            data : None,
+            slabs : None,
+            tree : None
+        }
+    }
+}
+
+impl SearchStructure {
+
     /**
      * For each line going from it upwards that has not yet
      * been covered, draw a line to it. The upper point of
@@ -14,7 +40,6 @@ impl library_types::SearchStructure {
      * selected line.
      *
      * ```
-     *
      *  -----b-----| level 3
      *       |     |
      *  ---a-------| level 2
@@ -24,38 +49,36 @@ impl library_types::SearchStructure {
      * [level 1] -> add line(c, a), line(c, b)
      * [level 2] -> remove line(c, a)
      * [level 3] -> remove line(c, b)
-     * ```
-     * ```
-     * b------ level 3
-     * |
-     *'b c     level 2
-     * |/
-     * a------ level 1
+     *
+     *  b------ level 3
+     *  |
+     * 'b c     level 2
+     *  |/
+     *  a------ level 1
      * ```
      * In this picture, line ab should be added when we process level 1,
      * and removed when we process level 3 (note: not on level 2, though
      * at level 2 line a'b still exists, where 'b is the intersection of
      * level 2 and ab.
      *
-     * The level contains the indices of all the lines in data
-     * that start from the y-level that is being currently processed
-     * one of the ideas is to accept the line from input and
-     * not shorten it when we are processing multiple levels
-     * like this;
+     * @param{level} contains the indices of all the lines in @field{data}
+     * that start from the y-level that is being currently processed.
+     * One of the ideas is to accept the line from input and not shorten
+     * it when we are processing multiple levels.
      */
 
     fn accept_next_level (&mut self, level: &mut[usize]) {
-        for index in level.iter() {
-
+        for _index in level.iter() {
+            let _line : &types::CompLine = &self.data.as_ref().unwrap()[level[0]];
+            self.tree.as_mut().unwrap().insert(*_line);
         }
     }
 }
 
 #[allow(unused)]
 fn preprocess(points : &mut Vec<Coordinate<f64>>) {
-    let mut search_structure =
-        library_types::SearchStructure{..Default::default()};
-    search_structure.accept_next_level(&mut[0]);
+    let mut search_structure = SearchStructure{..Default::default()};
+    //search_structure.accept_next_level(&mut[0]);
 }
 
 fn main() {
